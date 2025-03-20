4632f3d9d1e8a154cfe86d1245290f7a0a1f0cc3
Nalin Cooper <nic4340@g.rit.edu>
2025-03-20 19:34:34 -0400

    test

--- app.js
+++ app.js
@@ -277,6 +277,6 @@ function createMuseumCard(){
         <p class="about"> ${museum.Description}
     </div>`;
     
-    document.getElementsByClassName("card").insertAdjacentHTML("beforeend",template); 
+    document.querySelectorAll("card").insertAdjacentHTML("beforeend",template); 
 }
 createMuseumCard();
